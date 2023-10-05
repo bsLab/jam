7516bd454ddb9046b1d69c9f999dd067e1696176
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 16:22:16 +0200

    Thu 05 Oct 2023 04:20:06 PM CEST

---
+++ js/ml/dt.js
@@ -0,0 +1,35 @@
+/**
+ **      ==============================
+ **       O           O      O   OOOO
+ **       O           O     O O  O   O
+ **       O           O     O O  O   O
+ **       OOOO   OOOO O     OOO  OOOO
+ **       O   O       O    O   O O   O
+ **       O   O       O    O   O O   O
+ **       OOOO        OOOO O   O OOOO
+ **      ==============================
+ **      Dr. Stefan Bosse http://www.bsslab.de
+ **
+ **      COPYRIGHT: THIS SOFTWARE, EXECUTABLE AND SOURCE CODE IS OWNED
+ **                 BY THE AUTHOR(S).
+ **                 THIS SOURCE CODE MAY NOT BE COPIED, EXTRACTED,
+ **                 MODIFIED, OR OTHERWISE USED IN A CONTEXT
+ **                 OUTSIDE OF THE SOFTWARE SYSTEM.
+ **
+ **    $AUTHORS:     Ankit Kuwadekar, Stefan Bosse
+ **    $INITIAL:     (C) 2014, Ankit Kuwadekar
+ **    $MODIFIED:    (C) 2006-2018 bLAB by sbosse
+ **
+ **    $INFO:
+ **
+ ** ICE: C45/ID3 Decision Tree Algorithm supporting feature variables with eps intervals
+ **
+ ** Portable model
+ **
+ ** TODO: independent eps for each feature variable 
+ **        typeof eps = number | [epsx1:number,epsx2:number,..]
+ **
+ **    $ENDOFINFO
+ */
+module.exports = Require('plugins/ml/ice')
+
