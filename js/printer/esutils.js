1f0bb6f24dcd8dc0c4244bb72a886cbf7b9b9428
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 16:50:13 +0200

    Thu 05 Oct 2023 04:20:06 PM CEST

---
+++ js/printer/esutils.js
@@ -0,0 +1,9 @@
+var ast = Require('printer/ast');
+var code = Require('printer/code');
+var keyword = Require('printer/keyword');
+
+module.exports = {
+  ast:ast,
+  code:code,
+  keyword:keyword
+}
