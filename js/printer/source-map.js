f9bf99f09b652c1c40e7e10c036305a024c7a7c4
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 16:50:02 +0200

    Thu 05 Oct 2023 04:20:06 PM CEST

---
+++ js/printer/source-map.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2009-2011 Mozilla Foundation and contributors
+ * Licensed under the New BSD license. See LICENSE.txt or:
+ * http://opensource.org/licenses/BSD-3-Clause
+ */
+exports.SourceMapGenerator = Require('printer/lib/source-map-generator').SourceMapGenerator;
+exports.SourceMapConsumer = Require('printer/lib/source-map-consumer').SourceMapConsumer;
+exports.SourceNode = Require('printer/lib/source-node').SourceNode;
