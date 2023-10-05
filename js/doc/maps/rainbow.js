7d5ba11554f7fc288c4c9f772bd2a9c710fd92d4
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 16:31:16 +0200

    Thu 05 Oct 2023 04:20:06 PM CEST

---
+++ js/doc/maps/rainbow.js
@@ -0,0 +1,13 @@
+var colors = Require('doc/colors');
+
+module['exports'] = (function () {
+  var rainbowColors = ['red', 'yellow', 'green', 'blue', 'magenta']; //RoY G BiV
+  return function (letter, i, exploded) {
+    if (letter === " ") {
+      return letter;
+    } else {
+      return colors[rainbowColors[i++ % rainbowColors.length]](letter);
+    }
+  };
+})();
+
