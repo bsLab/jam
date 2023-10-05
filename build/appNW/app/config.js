a322f7e23076635d4f7816986b5cfe69f70ac73c
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 17:29:06 +0200

    Thu 05 Oct 2023 05:28:41 PM CEST

---
+++ build/appNW/app/config.js
@@ -0,0 +1,43 @@
+jamConfig={
+  "startJam": true,
+  "startNetwork": false,
+  "loadAgents": [
+    "chat.js"
+  ],
+  "startAgents": {
+    "chat": {
+      "demo": false
+    }
+  },
+  "network": [
+    {
+      "ip": "localhost",
+      "ipport": 10001,
+      "enable": true
+    },
+    {
+      "ip": "192.168.0.100",
+      "ipport": 10001,
+      "enable": true
+    },
+    {},
+    {}
+  ],
+  "capabilities": [
+    "[CC:66:A9:4F:6F:6A](0(FF)[9E:78:D8:CF:13:53])",
+    "",
+    "",
+    ""
+  ],
+  "log": {
+    "agent": true,
+    "parent": false,
+    "time": false,
+    "Time": true,
+    "class": false
+  },
+  "default": false,
+  "verbose": 0,
+  "popupLevel": 1,
+  "version": "1.1.36M"
+}
