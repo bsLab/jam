812fdbe0281b9c5b7f3d2cf690afa3504c6a4111
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-05 16:23:56 +0200

    Thu 05 Oct 2023 04:20:06 PM CEST

---
+++ js/dos/test/compat.fs
@@ -0,0 +1,11 @@
+var Io = require('../io');
+var Net = require('../network');
+var Sch = require('../scheduler');
+var Conn = require('../connection');
+var Rpc = require('../rpc');
+var Std = require('../std');
+var Afs = require('../afs');
+var Router = require('../router');
+var util = require('util');
+var Array = require('../compat').array;
+
